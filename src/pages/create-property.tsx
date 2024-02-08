1dd2956633889148823a4a5ffa3c5dfa5bf1be23
Ogalo <ogaloharrison@gmail.com>
2024-02-08 03:43:34 +0300

    Format the create property

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -12,17 +12,18 @@ const CreateProperty = () => {
 
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
 
-  const handleImageChange = (file: File) =>{
-    const reader = (readFile: File) => new Promise<string> ((resolve, reject) =>{
-      const fileReader = new FileReader();
-      fileReader.onload = () => resolve(fileReader.result as string);
-      fileReader.readAsDataURL(readFile);
-    });
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) =>
+        new Promise<string>((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.onload = () => resolve(fileReader.result as string);
+            fileReader.readAsDataURL(readFile);
+        });
 
-    reader(file).then((result: string) => setPropertyImage({
-      name: file?.name, url: result
-    }));
-  };
+    reader(file).then((result: string) =>
+        setPropertyImage({ name: file?.name, url: result }),
+    );
+};
 
   const onFinishHandler = async(data: FieldValues) => {
     if(!propertyImage.name) return alert('Please select an image');
