89eadd5e9dd52dd8a31769148b7f8e33ec556922
Ogalo <ogaloharrison@gmail.com>
2024-02-15 05:26:36 +0300

    Edit the URL on the data provider

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://boma-server.onrender.com/api/v1")}
+          dataProvider={dataProvider("https://boma-server.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
