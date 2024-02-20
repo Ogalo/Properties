e00a03a4e6195b56027cb44e0bec679eabbc0065
Ogalo <ogaloharrison@gmail.com>
2024-02-20 04:20:54 +0300

    Change the site URL

--- src/pages/my-profile.tsx
+++ src/pages/my-profile.tsx
@@ -1,15 +1,14 @@
-import { useGetIdentity, useOne } from "@pankod/refine-core";
+import { useGetIdentity, useOne } from '@pankod/refine-core'
 
 import { Profile } from "components";
 
 const MyProfile = () => {
     const { data: user } = useGetIdentity({
-
     });
     const { data, isLoading, isError } = useOne({
-        resource: 'users',
-        id: user?.userid
-    })
+        resource: "users",
+        id: user?.userid,
+    });
 
     const myProfile = data?.data ?? [];
 
